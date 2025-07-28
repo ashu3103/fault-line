5e6574d81f8e18aea810f8cff0c43005ed8c73f4
ashu3103 <ash2003sharma@gmail.com>
2025-07-29 00:29:57 +0530

    [#7] handle_print_string done

--- src/print.c
+++ src/print.c
@@ -6,15 +6,18 @@
 
 #include "print.h"
 
-static void handle_print_string(char* dst, size_t n, size_t curr_off, char* src);
-static void handle_print_integer(char* dst, size_t n, size_t curr_off, int src);
-static void handle_print_adress(char* dst, size_t n, size_t curr_off, void* src);
+/**
+ * Unbuffered version of vprintf
+ */
+static void handle_print_string(char* src);
+static void handle_print_integer(int src);
+static void handle_print_adress(void* src);
 
 static size_t strlength(const char* s);
-static void vprint(char* s, size_t n, const char* format_string, ...);
+static void vprint(const char* format_string, ...);
 
 static void
-vprint(char* s, size_t n, const char* format_string, ...)
+vprint(const char* format_string, ...)
 {
     size_t fs_length = 0;
     size_t va_count = 0;
@@ -24,22 +27,9 @@ vprint(char* s, size_t n, const char* format_string, ...)
     /* find the length of format string */
     fs_length = strlength(format_string);
 
-    /* derive the number of arguments in vr */
-    for (size_t i = 0; i < fs_length; i++)
-    {
-        if (format_string[i] == '%')
-        {
-            if (i + 1 < fs_length)
-            {
-                return;
-            }
-            va_count++;
-        }
-    }
-
-    va_start(args, va_count);
+    va_start(args, format_string);
     /* iterate through each character in format strig and accomodate it in buffer */
-    for (size_t i = 0; i < fs_length, curr_offset < n; i++)
+    for (size_t i = 0; i < fs_length; i++)
     {
         /* encountered a formatter character */
         if (format_string[i] == '%')
@@ -49,15 +39,15 @@ vprint(char* s, size_t n, const char* format_string, ...)
             {
                 /* string */
                 case 's':
-                    handle_print_string(s, n, curr_offset, va_arg(args, char*));
+                    handle_print_string(va_arg(args, char*));
                     break;
                 /* integer as base 10 */
                 case 'd':
-                    handle_print_integer(s, n, curr_offset, va_arg(args, int));
+                    handle_print_integer(va_arg(args, int));
                     break;
                 /* address */
                 case 'a':
-                    handle_print_adress(s, n, curr_offset, va_arg(args, void*));
+                    handle_print_adress(va_arg(args, void*));
                     break;
                 default:
                     va_end(args);
@@ -68,8 +58,8 @@ vprint(char* s, size_t n, const char* format_string, ...)
             continue;
         }
 
-        s[curr_offset] = format_string[i];
-        curr_offset++;
+        // Print the character encountered
+        write(1, format_string + i, sizeof(char));
     }
 
     va_end(args);
@@ -92,64 +82,67 @@ strlength(const char* s)
 }
 
 static void 
-handle_print_string(char* dst, size_t n, size_t curr_off, char* src)
+handle_print_string(char* src)
 {
     size_t src_len = strlength(src);
-
-    for (size_t j = 0; j < src_len, curr_off < n; j++)
-    {
-        dst[curr_off++] = src[j];
-    }
+    write(1, src, sizeof(char)*src_len);
 }
 
 static void
-handle_print_integer(char* dst, size_t n, size_t curr_off, int src)
+handle_print_integer(int src)
 {
-    if (src == 0)
-    {
-        dst[curr_off++] = '0';
-        return;
-    }
+    // if (src == 0)
+    // {
+    //     dst[curr_off++] = '0';
+    //     return;
+    // }
 
-    if (src < 0)
-    {
-        dst[curr_off++] = '-';
-        src = src*(-1);
-    }
+    // if (src < 0)
+    // {
+    //     dst[curr_off++] = '-';
+    //     src = src*(-1);
+    // }
 
-    while(curr_off < n && src > 0)
-    {
-        dst[curr_off++] = (src % 10) + '0';
-        src /= 10;
-    }
+    // while(curr_off < n && src > 0)
+    // {
+    //     dst[curr_off++] = (src % 10) + '0';
+    //     src /= 10;
+    // }
 }
 
 static void
-handle_print_adress(char* dst, size_t n, size_t curr_off, void* src)
+handle_print_adress(void* src)
 {
-    const char hex_digits[] = "0123456789abcdef";
-    uintptr_t sr_addr = (uintptr_t)src;
-    int num_nibbles = 0;
-    int started = 0;
+    // const char hex_digits[] = "0123456789abcdef";
+    // uintptr_t sr_addr = (uintptr_t)src;
+    // int num_nibbles = 0;
+    // int started = 0;
 
-    /* print the 0x to represent pointer in hex */
-    dst[curr_off++] = '0';
-    if (curr_off < n) return;
-    dst[curr_off++] = 'x';
-    if (curr_off < n) return;
+    // /* print the 0x to represent pointer in hex */
+    // dst[curr_off++] = '0';
+    // if (curr_off < n) return;
+    // dst[curr_off++] = 'x';
+    // if (curr_off < n) return;
 
-    num_nibbles = 2 * sizeof(uintptr_t);
+    // num_nibbles = 2 * sizeof(uintptr_t);
 
-    for (int shift = (num_nibbles - 1)*4; shift >= 0; shift -= 4)
-    {
-        char digit = (sr_addr >> shift) & 0xF;
+    // for (int shift = (num_nibbles - 1)*4; shift >= 0; shift -= 4)
+    // {
+    //     char digit = (sr_addr >> shift) & 0xF;
 
-        if (digit != 0 || started || shift == 0)
-        {
-            dst[curr_off++] = hex_digits[digit];
-            if (curr_off < n) return;
-            started = 1;
-        }
-    }
+    //     if (digit != 0 || started || shift == 0)
+    //     {
+    //         dst[curr_off++] = hex_digits[digit];
+    //         if (curr_off < n) return;
+    //         started = 1;
+    //     }
+    // }
 
+}
+
+
+int main()
+{
+    vprint("asdh%su\n", "haha");
+    return 0;
 }
